bb3357516a445ce92453fa04bbbc2abe2d404d1d
Ezekiel blanc <ezekielblanc1@gmail.com>
2019-01-08 13:06:11 +0100

    Setup the database

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express');
-const app = express()
+const app = express();
+const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRoute = require('./routes/index')
+const userRoute = require('./routes/user')
+
+//Setting up EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs")
+
+
+
+//Routes
+app.use('/', indexRoute)
+app.use('/user', userRoute)
+
+
+mongoose.connect('mongodb://localhost:27017/Authentication', {useNewUrlParser: true})
+  .then(() => console.log('Connected to database successfully'))
+  .catch((err) => console.log(err))
 
 
 
