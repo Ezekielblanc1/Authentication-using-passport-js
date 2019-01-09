bc7f48ebc95f843d2a8a67622bdbfba3e4936147
Ezekiel blanc <ezekielblanc1@gmail.com>
2019-01-09 17:23:15 +0100

    Added the user details to be displayed on the dashboard

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const passport = require('passport')
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash')
 const session = require('express-session')
 const indexRoute = require('./routes/index')
 const userRoute = require('./routes/user')
+
+require('./config/passport')(passport)
+
 
 //Setting up EJS
 app.use(expressLayouts);
@@ -18,13 +22,21 @@ app.use(session({
   secret: "my secret",
   resave: false,
   saveUninitialized: false
-}))
+}));
+
+
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Connect flash
 app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user
   next()
 })
 
@@ -42,3 +54,4 @@ mongoose.connect('mongodb://localhost:27017/Authentication', {useNewUrlParser: t
 
 const PORT = process.env.PORT ||4001
 app.listen(PORT, () => console.log(`App running on port ${PORT}` ))
+
